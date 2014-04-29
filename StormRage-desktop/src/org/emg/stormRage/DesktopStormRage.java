90ae3bb5aae61045ae91d6ee4cc5bc2f3c324573
Marcos159 <marcos.e3012@gmail.com>
2014-04-29 20:00:54 +0200

    Camara Arreglada
    La camara ya se mueve con el personaje y los npcs estan quietos

--- StormRage-desktop/src/org/emg/stormRage/DesktopStormRage.java
+++ StormRage-desktop/src/org/emg/stormRage/DesktopStormRage.java
@@ -1,9 +1,13 @@
 package org.emg.stormRage;
 
+
+
 import org.emg.stormRage.util.Constants;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+
 
 public class DesktopStormRage {
 	public static void main(String[] args) {
